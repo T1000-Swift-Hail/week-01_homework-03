2ba57927280801443e759fcc53e54f61bccca7fe
hanaalmutlak <Hana.Khamis.has.s@tuwaiq.edu.sa>
2021-10-04 17:07:50 +0300

    Done

---
+++ almutlak.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let mylastname: String = "hana"
+print(mylastname)
+var myage: Int = 20
+let HJGH: String = " Mylastname \(mylastname) ,and my is \(myage)"
+print(HJGH)
