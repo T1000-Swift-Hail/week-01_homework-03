60ad36011f74500da84e63693468b1c94b7b42a7
noyer altamimi <noyer.faisal.has.s@tuwaiq.edu.sa>
2021-10-04 17:11:18 +0300

    idonehomework

---
+++ MyLastName.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var noyer = "altamimi"
+print(noyer)
+var myage = 27
+print(myage)
+var me : String = "my last name is \(noyer), and i am \(myage) yers old"
+print(me)
+
+
+
