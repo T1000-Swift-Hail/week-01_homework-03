bddc54ce36770fb6293ead813910c730090bd2c6
Hind Alharbi <hindalharbi@Hinds-MacBook-Pro.local>
2021-10-04 16:36:09 +0300

    HW3 Done

---
+++ MyLastName.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let lastName: String = "Alharbi"
+var age: Int = 20
+var combined: String = "My last name is \(lastName) and I am \(age) old "
+print(lastName)
+print(combined)
+
+
