9b757ed3c3479c76dc2c1e39ad166083925c4605
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-10-04 17:01:11 +0300

    complete homework03

---
+++ MyLastName.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let mylastname: String = "alanazi"
+print (mylastname)
+var myage :Int = 30
+var me : String = "\(mylastname) , \(myage)"
+print (me)
