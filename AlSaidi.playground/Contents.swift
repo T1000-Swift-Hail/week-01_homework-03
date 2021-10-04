75510925d453c56aeb0d20e132a4a3f67169352f
dalal92 <dalal@MacBook-Pro.local>
2021-10-04 16:08:52 +0300

    HomeWork2 is Done

---
+++ AlSaidi.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let myLastName: String = "AlSaidi"
+
+print(myLastName)
+
+let myAge: Int = 28
+print("My Last name is \(myLastName) and I am \(myAge) years old")
+      
