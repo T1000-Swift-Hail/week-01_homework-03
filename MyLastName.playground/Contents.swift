2f96dda4a82687c8d8d984b4c9f3c5ce00ff94f0
Anas Hamad <anashamad@MacBook-Air-Anas.local>
2021-10-04 16:22:17 +0300

    complete HW 3

---
+++ MyLastName.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let MyLastName: String = "Alhumaid"
+
+var MyAge: Int = 29
+
+var compaine = ("My last name",(MyLastName),"and I am ",(MyAge))
+
+print(compaine)
+
