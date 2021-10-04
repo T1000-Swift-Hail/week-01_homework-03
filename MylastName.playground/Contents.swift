67e052cb23f8d0734292fb30b507e1b70cc4b0a4
hesah <hesah.raja.has.s@tuwaiq.edu.sa>
2021-10-04 16:48:46 +0300

    done home work3

---
+++ MylastName.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let myLastname: String = "Hesah"
+
+var myAge: Int = 28
+
+print("my Last name is \(myLastname) and i am \(myAge)")
+
+print(myAge)
