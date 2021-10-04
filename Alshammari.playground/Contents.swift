840cb71a74b70ae9d8256997695938efce85095a
azoz <aabdoalaziz1@gmail.com>
2021-10-04 16:51:42 +0300

    Homework#3 Done

---
+++ Alshammari.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Foundation
+
+var mylastname : String = "Alshammari"
+
+
+print(mylastname)
+
+
+let myInteger : Int = 28
+
+let age : Int = 28
+
+var combind: String = "my last name is \(mylastname) , \(myInteger) years old" 
+
+print(combind)
+
