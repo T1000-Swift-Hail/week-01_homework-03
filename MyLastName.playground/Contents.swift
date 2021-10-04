3ce9a1b539d64c348194e22d04a3b0b4bc92a5cb
Majed Alshammari <admin@Majeds-MacBook-Air.local>
2021-10-04 16:30:24 +0300

    complete homework week3

---
+++ MyLastName.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let LastName: String = "Alshammari"
+
+print(LastName)
+
+var MyAge: Int = 24
+
+print("My last name is \(LastName) and I'm \(MyAge) years old")
