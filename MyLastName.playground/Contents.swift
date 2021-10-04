e44fb8e29d36b38b944f31620eb8f18b1f562b8d
Njoud Alrshidi <njoudalrshidi@MacBook-Pro-Njoud.local>
2021-10-04 16:53:28 +0300

    HW3DONE

---
+++ MyLastName.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let myLastName: String = "Alrasheedi"
+print (myLastName)
+
+
+var myAge: Int = 24
+print (myAge)
+print ("My Last Name is \(myLastName) and I am \(myAge) years old")
