f48991672295088bd9befbd7e9f08f0c41e5fc23
Mastorah <mas@Mastorahs-MacBook-Air.local>
2021-10-04 17:14:11 +0300

    done home work 3

---
+++ MyLastName.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let Mylastname: String = "alzamil  "
+print (Mylastname)
+var myAge: Int = 28
+print (myAge)
+print ("My last name is \(Mylastname) And I am \(myAge) years old ")
